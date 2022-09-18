fca8d82b2bc369376d4aa76e768bead2cd88e0a4
andersonsaj <andersonsb09@hotmail.com>
2022-09-18 13:48:34 -0300

    Challenge finalized

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,45 @@
-export function SideBar() {
-  // Complete aqui
+import { ButtonHTMLAttributes, useState, useEffect } from 'react';
+import { Button } from '../components/Button';
+import { api } from '../services/api';
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SiderBarProps extends ButtonHTMLAttributes<HTMLButtonElement>  {
+  selectedGenreId: number;
+  onHandleClickButton: (id: number) => void;
+}
+
+export function SideBar({selectedGenreId, onHandleClickButton, ...rest}: SiderBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            selected={selectedGenreId === genre.id}
+            onClick={() => onHandleClickButton(genre.id)}
+            {...rest}
+          />
+          ))}   
+        </div>
+
+    </nav>
+  );
 }
